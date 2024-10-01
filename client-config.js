ca7464ad4e59eefab9c511c50e2a09d28e787a78
David Bainbridge <davidb@waikato.ac.nz>
2024-10-01 17:35:20 +1300

    Files to help auto-script configuring BITW

---
+++ client-config.js
@@ -0,0 +1,7 @@
+const config = {
+    CESIUM_API_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxNGEzNDI4YS04MGY5LTQ5YWUtOTkzOC0xZDI4YjYzZDM2MzMiLCJpZCI6MjM1MDYzLCJpYXQiOjE3MjM3NjA5NTF9.QBOF8M32sANRPcZUD8fWlM2bqh7LsBlMqjI18lI72d0",
+    WEATHER_API_KEY: "1a51a1358e149e76be1fde4d7c790322",
+    SERVER_URL: "https://bitw-server.so-we-must-think.space"
+}
+
+export default config
