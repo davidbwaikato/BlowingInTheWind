940993adec2d5f098cde43de2c47046b9a2c8ef0
David Bainbridge <davidb@waikato.ac.nz>
2024-10-01 17:58:10 +1300

    Further tweaks/tidy-up

--- client/BITW-START-CLIENT.sh
+++ client/BITW-START-CLIENT.sh
@@ -4,6 +4,5 @@ if [ "x$BITW_HOME" = "x" ] ; then
     cd .. && source ./SETUP.bash && cd client
 fi
 
-echo "**** HOME=$BITW_HOME"
 
 PORT=$BITW_CLIENT_PORT WDS_SOCKET_PORT=$BITW_SERVER_WDS_SOCKET_PORT npm run start
--- server/BITW-START-SERVER.sh
+++ server/BITW-START-SERVER.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+if [ "x$BITW_HOME" = "x" ] ; then
+    cd .. && source ./SETUP.bash && cd client
+fi
 
 npm run start
